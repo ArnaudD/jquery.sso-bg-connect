33a33fb73a498ff07a275c9c3adc5e80183ef85d
Arnaud Didry <arnaud@didry.info>
2011-03-10 18:31:13 +0100

    Rough draft

---
+++ jquery.sso-bg-connect.js
@@ -0,0 +1,70 @@
+(function ($){
+
+  var loadIframe = function (url, id) {
+        $(document).append ('<iframe id="ssoBgConnect_'+id+'" src="'+url+'" width="1px" height="1px" style="visibility: hidden;"></iframe>');       
+
+  }
+
+  var settings = {
+    services: {},
+    success: '',
+    servicesHandlers: {
+      generic: function (serviceConf) {
+        loadIframe (service.url.replace ('{{callback}}', encodeURIComponent (settings.success)), );
+      },
+      facebook: function (serviceConf) {
+        /* TODO */
+      },
+      twitter: function (serviceConf) {
+        /* TODO */
+      },
+    }
+  };
+
+  var methods = {
+
+    /**
+     * Main method
+     */
+    connect: function (options) {
+
+      if (options) { 
+        $.extend( settings, options );
+      }
+
+      // For each service call the appropriate handler
+      $.each (settings.services, function (index, service) {
+        if (settings.servicesHandlers [service.url])
+          settings.servicesHandlers [service.url] (service);
+        else 
+          settings.servicesHandlers.generic (service);
+      });
+
+    },
+
+    /**
+     * Method called once the user has been authenticated
+     */
+    finishAuthentication: function (options) { 
+      if (typeof settings.success === 'function')
+        settings.success (options);
+      else if (typeof settings.success.length > 0)
+        document.location.href = settings.success ;
+      else
+        history.go(0);
+    },
+  };
+
+  $.fn.ssoBgConnect = function (method) {
+    
+    if ( methods [method] ) {
+      return methods [method].apply (this, Array.prototype.slice.call (arguments, 1));
+    } else if (typeof method === 'object' || ! method) {
+      return methods.connect.apply (this, arguments);
+    } else {
+      $.error ('Method ' +  method + ' does not exist on jQuery.ssoBgConnect');
+    }    
+  
+  };
+
+}) (jQuery);
